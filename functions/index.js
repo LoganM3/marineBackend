048adf2e7ea7eca56d8644497aa1e0e9c26b36cd
Logan McCalley <mccalleylogan@gmail.com>
2022-12-06 13:53:15 -0500

    cors error

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-// import functions from 'firebase-functions'
+import functions from 'firebase-functions'
 import express from 'express'
 import cors from 'cors'
 import { getAllAudio } from './src/routes/audio.js'
@@ -13,15 +13,15 @@ app.use(cors())
 app.use(express.json())
 
 
-app.get('/JL', getAllAudio)
+app.get('/JL-Audio', getAllAudio)
 
-app.get('/',(req,res) =>{
-    res.status(200).send('hello wowrld')
-})
+// app.get('/',(req,res) =>{
+//     res.status(200).send('hello wowrld')
+// })
 
-const port = 4000
-app.listen(port, () => {
-    console.log('ready on http://localhost:'+port)
-})
+// const port = 4000
+// app.listen(port, () => {
+//     console.log('ready on http://localhost:'+port)
+// })
 
-// export const api = functions.https.onRequest(app)
+export const api = functions.https.onRequest(app)
--- functions/src/routes/audio.js
+++ functions/src/routes/audio.js
@@ -2,10 +2,10 @@ import dbConnect from "../dbConnect.js";
 
 export async function getAllAudio(req, res) {
     const db = dbConnect();
-    const collection = await db.collection("JL-speakers").find().toArray()
-      .catch(err => {
-        res.status(500).send(err);
-        return;
-      });
+    const collection = await db.collection("jlSpeakers").find().toArray()
+    //   .catch(err => {
+    //     res.status(500).send(err);
+    //      return;
+    //   });
     res.send(collection);
   }
